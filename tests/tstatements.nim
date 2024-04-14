f89437a40264bd03093cea319684a15c1f624391
Buldram <buldram@proton.me>
2024-04-14 14:07:48 -0400

    Add test for tuple unpacking

--- tests/tstatements.nim
+++ tests/tstatements.nim
@@ -1,6 +1,6 @@
 import ../skulite
 
-let db = openDatabase(":memory:")
+var db = openDatabase(":memory:")
 db.exec "CREATE TABLE IF NOT EXISTS example(words TEXT) STRICT"
 let s1 = db.prepStatement("INSERT INTO example (words) VALUES (?),(?)", ("Hello,", "world!"))
 doAssert s1.sql == "INSERT INTO example (words) VALUES (?),(?)"
@@ -23,3 +23,12 @@ doAssert s2.readonly
 step s2
 doAssert s2.numValues == 1
 explain(s2)
+
+db.reopen(":memory:")
+db.exec "CREATE TABLE IF NOT EXISTS example(a TEXT, b TEXT) STRICT"
+db.exec "INSERT INTO example(a,b) VALUES (?,?)", ("Hello,", "World!")
+let s3 = db.prepStatement("SELECT a,b FROM example")
+explain(s3)
+step s3
+doAssert unpack(s3, (string, string)) == ("Hello,", "World!")
+doAssert db.query("SELECT a,b FROM example", (string, string)) == ("Hello,", "World!")
