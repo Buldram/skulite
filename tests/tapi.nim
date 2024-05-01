e7a1cff4a40e34a74d29b94c72ca96fea5716b6b
Buldram <buldram@proton.me>
2024-05-01 04:42:58 -0400

    Test for custom `SQlite3_destructor`

--- tests/tapi.nim
+++ tests/tapi.nim
@@ -1,17 +1,23 @@
 import ../skulite
-
-proc readonly(stmt: Statement): bool {.importc: "sqlite3_stmt_readonly", noconv.}
-let db = openDatabase(":memory:")
-let s1 = db.prepStatement "CREATE TABLE IF NOT EXISTS test(words)"
-doAssert not readonly(s1)
-exec s1
-let s2 = db.prepStatement "SELECT * from test"
-doAssert readonly(s2)
-
-
 from ../skulite/sqlite3c import sqlite3_bind_text
 
-proc one(s: pointer) {.noconv.} =
-  echo 1
+proc tapi {.inline.} =
+  let db = openDatabase(":memory:")
 
-discard sqlite3_bind_text(s1, 1, "", 0, one)
+  block:
+    proc readonly(stmt: Statement): bool {.importc: "sqlite3_stmt_readonly", noconv.}
+    let s1 = db.prepStatement "CREATE TABLE IF NOT EXISTS test(words)"
+    doAssert not readonly(s1)
+    exec s1
+    let s2 = db.prepStatement "SELECT * from test"
+    doAssert readonly(s2)
+
+  block:
+    var v {.global.} = 0
+    block:
+      let s = db.prepStatement "CREATE TABLE IF NOT EXISTS test(words)"
+      proc one(_: pointer) {.noconv.} =
+        v = 1
+      discard sqlite3_bind_text(s, 1, "", 0, one)
+    doAssert v == 1
+tapi()
